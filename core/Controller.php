f9de4418420e1e1e5a6727c0a31d65a28df3a090
Dovtutis <dovtutis@gmail.com>
2021-03-01 12:30:30 +0200

    Controller setLayout and renderView functions done

--- core/Controller.php
+++ core/Controller.php
@@ -12,5 +12,23 @@ namespace app\core;
  */
 class Controller
 {
+    public string $layout = 'main';
 
+    public function setLayout(string $layout)
+    {
+        $this->layout = $layout;
+    }
+
+    /**
+     * We render the base view with params
+     *
+     * @param string $view
+     * @param array $params
+     * @return string|string[]
+     */
+
+    public function render($view, $params = [])
+    {
+        return Application::$app->router->renderView($view, $params);
+    }
 }
