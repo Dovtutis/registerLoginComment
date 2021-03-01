ef3295287e20fdfea1b367171cef2b31c594e493
Dovtutis <dovtutis@gmail.com>
2021-03-01 11:59:42 +0200

    Router.php resolve function in progress

--- core/Router.php
+++ core/Router.php
@@ -60,4 +60,37 @@ class Router
         $this->routes['post'][$path] = $callback;
     }
 
+    public function resolve(){
+        $path = $this->request->getPath();
+        $method = $this->request->method();
+
+        // Trying to run a route from router array
+        $callback = $this->routes[$method][$path] ?? false;
+
+        // If there is no such route added, we say not exist
+        if ($callback === false) :
+
+            $pathArr = explode('/', ltrim($path, '/'));
+
+            if (count($pathArr) === 2) :
+                $path = '/'.$pathArr[0];
+                $urlParam['value'] = $pathArr[1];
+            endif;
+
+            if (count($pathArr) === 3) :
+                $path = '/'. $pathArr[0] . '/'. $pathArr[1];
+                $urlParam['value'] = $pathArr[2];
+            endif;
+
+            $callback = $this->routes[$method][$path] ?? false;
+
+        endif;
+
+        if (is_string($callback)) :
+            // We will render the view
+        endif;
+
+        echo call_user_func($callback, $this->request, $urlParam ?? null);
+    }
+
 }
