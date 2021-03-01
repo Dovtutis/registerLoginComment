16fe95c25f47bf4d1b58381556ce75942f0eb8d8
Dovtutis <dovtutis@gmail.com>
2021-03-01 19:14:31 +0200

    Registration function in UserModel created and tested

--- model/UserModel.php
+++ model/UserModel.php
@@ -19,4 +19,28 @@ class UserModel
     {
         $this->db = Application::$app->db;
     }
+
+    /**
+     * Register function which creates SQL query, binds params and executes the query.
+     *
+     * @param $data
+     * @return bool
+     */
+    public function register($data)
+    {
+        $this->db->query("INSERT INTO users (`name`, `surname`, `email`, `password`, `phone`, `address`)
+            VALUES (:name, :surname, :email, :password, :phone, :password)");
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':surname', $data['surname']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':phone', $data['phone']);
+        $this->db->bind(':address', $data['address']);
+
+        if ($this->db->execute()){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
