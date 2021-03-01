b5e160a3e803242839d0b2f549644beb876b6c06
Dovtutis <dovtutis@gmail.com>
2021-03-01 12:47:19 +0200

    new Application initiated in index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,18 @@
 <?php
 require_once '../vendor/autoload.php';
 
+use app\controller\SiteController;
 use app\core\Application;
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
+
+$app = new Application(dirname(__DIR__), $config);
