4d604fb637e5962bf431f9f53e95ad3463d887b0
Dovtutis <dovtutis@gmail.com>
2021-03-01 21:20:57 +0200

    Logout rout added

--- public/index.php
+++ public/index.php
@@ -26,4 +26,6 @@ $app->router->post('/register', [AuthController::class, 'register']);
 $app->router->get('/login', [AuthController::class, 'login']);
 $app->router->post('/login', [AuthController::class, 'login']);
 
+$app->router->get('/logout', [AuthController::class, 'logout']);
+
 $app->run();
