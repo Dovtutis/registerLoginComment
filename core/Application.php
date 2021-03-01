6c67597b67456da044c893afa14e8dc40fadeff5
Dovtutis <dovtutis@gmail.com>
2021-03-01 12:38:36 +0200

    $response added to Application.php

--- core/Application.php
+++ core/Application.php
@@ -20,6 +20,7 @@ class Application
     public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
+    public Response $response;
     public static Application $app;
     public Controller $controller;
 
@@ -29,7 +30,8 @@ class Application
         self::$app = $this;
 
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
     }
 
     public function run()
