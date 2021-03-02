ab2a01bf465884522d4687af040aceed1c43afe2
Dovtutis <dovtutis@gmail.com>
2021-03-02 12:16:18 +0200

    getComments function added and tested

--- controller/FeedbackController.php
+++ controller/FeedbackController.php
@@ -6,6 +6,7 @@ namespace app\controller;
 
 use app\core\Controller;
 use app\core\Validation;
+use app\model\CommentsModel;
 use app\model\UserModel;
 
 /**
@@ -15,12 +16,14 @@ use app\model\UserModel;
 class FeedbackController extends Controller
 {
     public Validation $validation;
+    private CommentsModel $commentsModel;
     protected UserModel $userModel;
 
     public function __construct()
     {
         $this->validation = new Validation();
         $this->userModel = new UserModel();
+        $this->commentsModel = new CommentsModel();
     }
 
     public function index (){
@@ -29,5 +32,12 @@ class FeedbackController extends Controller
             'currentPage' => "feedback"
         ];
         return $this->render('feedback', $params);
+    }
+
+    public function getComments() {
+        $comments = $this->commentsModel->getComments();
+
+        header('Content-Type: application/json');
+        echo json_encode($comments);
     }
 }
